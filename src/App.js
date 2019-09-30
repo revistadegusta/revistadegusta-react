4d1c6abeeedeb28558c63b73f442ae402e6a1f64
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-09-30 20:13:36 -0300

    fix responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import sunset from './images/sunset.jpg';
+import sunset from './images/logo.jpg';
 import './App.scss';
 
 function App() {
   return (
     <div className="container-init d-flex flex-column justify-content-center">
-      <div className="align-self-center text-center">
+      <div className="text-center">
         <p>revista degusta</p>
         <hr />
       </div>
-      <div className="d-flex justify-content-center align-self-center text-center">
-        <div className="align-self-center text-center p-2">
+      <div className="text-center">
+        <div className="text-center p-2">
           <a href="https://www.instagram.com/revistadegusta/">instagram</a>
         </div>
-        <div className="align-self-center text-center p-2">
+        <div className="text-center p-2">
           <a href="https://www.youtube.com/channel/UCcRxoPEtYdUO5LQmZj7wPow">youtube</a>
         </div>
-        <div className="align-self-center text-center p-2">
+        <div className="text-center p-2">
           <a href="mailto:contato@revistadegusta.com?subject=Contato com a revista degusta&body=Olá, gostaria de saber mais sobre a revista. Valeu!">contato@revistadegusta.com</a>
         </div>
       </div>
