8aa83a5effb88fc65c75c133b09e6fb42ab7d475
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-09-30 19:17:16 -0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import sunset from './images/sunset.jpg';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-init d-flex flex-column justify-content-center">
+      <div className="align-self-center text-center">
+        <p>revista degusta</p>
+        <hr />
+      </div>
+      <div className="d-flex justify-content-center align-self-center text-center">
+        <div className="align-self-center text-center p-2">
+          <a href="https://www.instagram.com/revistadegusta/">instagram</a>
+        </div>
+        <div className="align-self-center text-center p-2">
+          <a href="https://www.youtube.com/channel/UCcRxoPEtYdUO5LQmZj7wPow">youtube</a>
+        </div>
+      </div>
     </div>
   );
 }
